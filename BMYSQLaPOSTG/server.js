bb811eb1a06c3628722e7c3b3c04576a56743892
davindalana <46117700+davindalana@users.noreply.github.com>
2025-08-20 16:25:09 +0700

    push cors

--- BMYSQLaPOSTG/server.js
+++ BMYSQLaPOSTG/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use('/api', apiRoutes);
